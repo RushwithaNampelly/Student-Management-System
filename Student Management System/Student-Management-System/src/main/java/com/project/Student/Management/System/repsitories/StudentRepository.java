0ae6e756c63f91091541e9a24e53eb67706c5e3a
Rushwitha Nampelly <154776183+RushwithaNampelly@users.noreply.github.com>
2025-12-03 17:53:18 +0530

    Delete Student Management System/Student-Management-System/src/main/java/com/project/Student/Management/System directory

--- Student Management System/Student-Management-System/src/main/java/com/project/Student/Management/System/repsitories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.Student.Management.System.repsitories;
-
-import com.project.Student.Management.System.models.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,Long> {
-}
